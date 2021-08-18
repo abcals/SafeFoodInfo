92de89a5f6916cc1464ef0339dc4bb7e76d8f686
abcals <abcals/my_test.git>
2021-08-18 15:34:39 +0900

    프로젝트 생성

---
+++ SafeFoodInfo/src/main/java/SafeFoodInfo/SafeFoodInfoApplication.java
@@ -0,0 +1,13 @@
+package SafeFoodInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeFoodInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeFoodInfoApplication.class, args);
+	}
+
+}
---
+++ SafeFoodInfo/src/test/java/SafeFoodInfo/SafeFoodInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package SafeFoodInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeFoodInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
